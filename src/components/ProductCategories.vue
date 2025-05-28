61a284d1f38582785da5e89d8f383787e4c64384
Sergio Twist <twist_tws@bk.ru>
2025-05-28 13:37:21 +0300

    small fix 2

--- src/components/ProductCategories.vue
+++ src/components/ProductCategories.vue
@@ -1,39 +1,61 @@
 <template>
   <div class="bg-[#FAFAFA]">
-    <div class="w-[1120px]">
-      <div class="flex justify-between">
-        <p>Browse By Category</p>
+    <div class="container mx-auto">
+      <div class="flex justify-between mb-10">
+        <p class="text-2xl font-bold">Browse By Category</p>
         <div class="flex">
           <ChevronLeft />
           <ChevronRight />
         </div>
       </div>
       <div class="grid grid-cols-6 gap-5">
-        <div class="bg-[#EDEDED] rounded-md">
-          <div class="px-6 py-14">
-            <Smartphone />
-            <span>Phones</span>
+        <div
+          class="bg-[#EDEDED] rounded-md flex justify-center items-center px-6 py-14"
+        >
+          <div>
+            <Smartphone class="mx-auto size-15 mb-5" />
+            <span class="text-2xl">Phones</span>
           </div>
         </div>
-        <div class="bg-[#EDEDED] rounded-md">
-          <Smartphone />
-          <span>Phones</span>
+        <div
+          class="bg-[#EDEDED] rounded-md flex justify-center items-center px-6 py-14"
+        >
+          <div>
+            <Watch class="mx-auto size-15 mb-5" />
+            <span class="text-2xl">Watches</span>
+          </div>
         </div>
-        <div class="bg-[#EDEDED] rounded-md">
-          <Smartphone />
-          <span>Phones</span>
+        <div
+          class="bg-[#EDEDED] rounded-md flex justify-center items-center px-6 py-14"
+        >
+          <div>
+            <Camera class="mx-auto size-15 mb-5" />
+            <span class="text-2xl">Cameras</span>
+          </div>
         </div>
-        <div class="bg-[#EDEDED] rounded-md">
-          <Smartphone />
-          <span>Phones</span>
+        <div
+          class="bg-[#EDEDED] rounded-md flex justify-center items-center px-6 py-14"
+        >
+          <div>
+            <Smartphone class="mx-auto size-15 mb-5" />
+            <span class="text-2xl">Phones</span>
+          </div>
         </div>
-        <div class="bg-[#EDEDED] rounded-md">
-          <Smartphone />
-          <span>Phones</span>
+        <div
+          class="bg-[#EDEDED] rounded-md flex justify-center items-center px-6 py-14"
+        >
+          <div>
+            <Smartphone class="mx-auto size-15 mb-5" />
+            <span class="text-2xl">Phones</span>
+          </div>
         </div>
-        <div class="bg-[#EDEDED] rounded-md">
-          <Smartphone />
-          <span>Phones</span>
+        <div
+          class="bg-[#EDEDED] rounded-md flex justify-center items-center px-6 py-14"
+        >
+          <div>
+            <Smartphone class="mx-auto size-15 mb-5" />
+            <span class="text-2xl">Phones</span>
+          </div>
         </div>
       </div>
     </div>
@@ -41,5 +63,11 @@
 </template>
 
 <script setup>
-import { ChevronLeft, ChevronRight, Smartphone } from 'lucide-vue-next';
+import {
+  Camera,
+  ChevronLeft,
+  ChevronRight,
+  Smartphone,
+  Watch,
+} from 'lucide-vue-next';
 </script>
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  theme: {
+    container: {
+      center: true,
+      padding: '1rem',
+      screens: {
+        sm: '640px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1120px',
+        '2xl': '1120px',
+      },
+    },
+  },
+
+  plugins: [],
+};
